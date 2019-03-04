b2a61d20cfa81d1d35388a2ba4a972027be5300d
torlax <torlax@gmail.com>
2019-03-04 18:47:32 +0000

    Changed portuguese string to english

--- store/app.py
+++ store/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/users/<username>")
 def hello(username):
-        return "ola {}".format(username)
+        return "Hello {}".format(username)
 
 
     app.run(host="0.0.0.0", port=80)
